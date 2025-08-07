4c1f09f88888c824974f6faeaac893249f50ab6b
celiors <celioxer@gmail.com>
2025-08-07 16:01:01 -0300

    Segunda Att

--- src/main/java/com/site/controllers/SiteController.java
+++ src/main/java/com/site/controllers/SiteController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SiteController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
-        return "home"; // o Spring procura por home.html em /templates
+        return "home.html"; // o Spring procura por home.html em /templates
     }
     @GetMapping("/episodios")
     public String episodios() {
@@ -17,11 +17,15 @@ public class SiteController {
 
     @GetMapping("/sobre")
     public String sobre() {
-        return "sobre";
+        return "sobre.html";
     }
 
     @GetMapping("/contato")
     public String contato() {
-        return "contato";
+        return "contato.html";
+    }
+    @GetMapping("/apoiadores")
+    public String apoiadores() {
+        return "apoiadores.html";
     }
 }
